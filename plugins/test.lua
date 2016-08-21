9bc06712f182463f01209cf0deb2ffea2f16f407
NavidQuick <navid.kakavand2@gmail.com>
2016-08-22 03:07:53 +0430

    Update test.lua

--- plugins/test.lua
+++ plugins/test.lua
@@ -1,17 +1,23 @@
-local triggers = {
-	'^/test'
-}
+local function run(msg)
+local database = 'http://mpfan.ir/'
+local function run(msg)
+local res = http.request(database.."joke.db")
+local joke = res:split(",")
+local jok = joke[math.random(#joke)]
+  local text = "*TeleDanger* `1.2An Advanced Administration Api Bot Based On TeleSeed Written In Lua` "
+    local keyboard = {}
+    keyboard.inline_keyboard = {
+   {
+               {text = 'در حال طراحی', },
+      },
+ send_api_keyboard(msg, get_receiver_api(msg), text, keyboard)
+  end
+return { 
+patterns = {
+"joke",
+}, 
+run = run
+ }
 
-local function on_each_msg(msg, ln)
-	return msg
-end
 
-local action = function(msg, blocks, ln)
-	--test your stuffs here
-end
 
-return {
-	action = action,
-	triggers = triggers,
-	test = true
-}
